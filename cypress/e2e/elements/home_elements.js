fbdc5f3eb7487ae4499bc24fff025da0aba73596
Tiago Mota <tmota.seg@gmail.com>
2025-11-23 00:19:55 -0300

    func: funções / mapenado os elementos ga Home e Login em Arrow Functions.

---
+++ cypress/e2e/elements/home_elements.js
@@ -0,0 +1,9 @@
+class HomeElements{
+    // Botão 'Hide' na Home 
+    btnHideCookie = () => { return '#cookie_stop'}
+    // Botão 'Agents', anterior ao Login
+    selectAccount = () => { return 'Agents'}
+    // Botão 'Login'
+    btnLogin = () => { return 'Login'}
+
+} export default HomeElements
