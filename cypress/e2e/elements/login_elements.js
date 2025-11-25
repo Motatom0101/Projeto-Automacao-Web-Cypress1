f54ebf12adc700046ea0a7c729431ea7975d4bb1
Tiago Mota <tmota.seg@gmail.com>
2025-11-24 23:26:02 -0300

    feat: class LoginElements / Mapeamento dos elementos da págima Login na classe LoginElements.

---
+++ cypress/e2e/elements/login_elements.js
@@ -0,0 +1,9 @@
+class LoginElements{
+    // Campo de inputEmail na tela Login 
+    inputEmail = () => { return '#email'}
+    // Campo de inputPassword
+    inputPassword = () => { return '#password'}
+    // Botão Login
+    btnLogin = () => { return '#submitBTN'}
+
+} export default LoginElements
