fed7823e88ca04f60ff6b26b0f4c4f19e99940c6
Tiago Mota <tmota.seg@gmail.com>
2025-12-08 22:37:12 -0300

    feat: elementos/Mapeando elementos da Home e Login.

--- cypress/e2e/pages/login_pages.js
+++ cypress/e2e/pages/login_pages.js
@@ -1,5 +1,32 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
+import HomeElements from "../elements/home_elements"
+import LoginElements from "../elements/login_elements"
+
+const loginElements = new LoginElements
+const homeElements = new HomeElements
+
+const url = Cypress.config('baseUrl')
 
 class LoginPage {
+
+    accessHomePage() {
+        cy.visit(url)
+        cy.wait(3000)
+        cy.get(homeElements.btnHideCookie()).click()
+    }
+
+    accessLoginPage() {
+        cy.contains(homeElements.selectAccount()).click
+        cy.contains(homeElements.btnLogin()).click
+    }
+
+    fillLoginInfo(email, password) {
+        cy.contains(loginElements.inputEmail()).type(email)
+        cy.get(loginElements.inputPassword()).type(password)
+    }
+
+    submitLogin() {
+        cy.get(loginElements.submitLogin()).click()
+    }
 
 } export default LoginPage
